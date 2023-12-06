1be2e026fbc072e32fd0828c1cc135bbd3ed30ea
Fakhrillo <dombit1999@gmail.com>
2023-12-06 15:13:07 +0500

    'new_admin'

--- Core/settings.py
+++ Core/settings.py
@@ -112,6 +112,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Define the URL for serving media files during development
 MEDIA_URL = 'media/'
@@ -128,3 +129,60 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # AWS_STORAGE_BUCKET_NAME = 'cameramanbucket'
 # AWS_QUERYSTRING_AUTH = False
 # AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+
+JAZZMIN_SETTINGS = {
+    # title of the window (Will default to current_admin_site.site_title if absent or None)
+    "site_title": "People Counting",
+
+    # Title on the login screen (19 chars max) (defaults to current_admin_site.site_header if absent or None)
+    "site_header": "P.C. Camera Configurations",
+
+    # Title on the brand (19 chars max) (defaults to current_admin_site.site_header if absent or None)
+    "site_brand": "Configurations",
+
+    # Logo to use for your site, must be present in static files, used for brand on top left
+    "site_logo": 'Frame 227.png',
+
+    # Logo to use for your site, must be present in static files, used for login form logo (defaults to site_logo)
+    "login_logo": 'logoRes.svg',
+
+    # CSS classes that are applied to the logo above
+    "site_logo_classes": "img-circle",
+
+    # Relative path to a favicon for your site, will default to site_logo if absent (ideally 32x32 px)
+    "site_icon": 'logo.svg',
+
+    # Welcome text on the login screen
+    "welcome_sign": "Welcome to the P.C. Camera Configuration",
+
+    # Copyright on the footer
+    "copyright": "Silkhub",
+
+    # Field name on user model that contains avatar ImageField/URLField/Charfield or a callable that receives the user
+    "user_avatar": None,
+
+    # Links to put along the top menu
+    "topmenu_links": [
+        # App with dropdown menu to all its models pages (Permissions checked against models)
+        {"app": "result"},
+    ],
+
+    # Custom icons for side menu apps/models See https://fontawesome.com/icons?d=gallery&m=free&v=5.0.0,5.0.1,5.0.10,5.0.11,5.0.12,5.0.13,5.0.2,5.0.3,5.0.4,5.0.5,5.0.6,5.0.7,5.0.8,5.0.9,5.1.0,5.1.1,5.2.0,5.3.0,5.3.1,5.4.0,5.4.1,5.4.2,5.13.0,5.12.0,5.11.2,5.11.1,5.10.0,5.9.0,5.8.2,5.8.1,5.7.2,5.7.1,5.7.0,5.6.3,5.5.0,5.4.2
+    # for the full list of 5.13.0 free icon classes
+    "icons": {
+        "auth": "fas fa-users-cog",
+        "auth.user": "fas fa-user-cog",
+        "result.ConfigParameter": "fas fa-sliders-h",
+        "result.StreamPhoto": "fas fa-image",
+        "result.tracked": "fas fa-chart-bar",
+    },
+}
+
+JAZZMIN_UI_TWEAKS = {
+    "theme": "flatly",
+    "sidebar_nav_small_text": True,
+    "footer_small_text": True,
+    "sidebar_fixed": True,
+
+}
