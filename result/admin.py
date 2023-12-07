62d674e827cb5b07b376e3be504da81e033e3f2d
Fakhrillo <dombit1999@gmail.com>
2023-12-07 17:37:38 +0500

    'new_admin'

--- result/admin.py
+++ result/admin.py
@@ -4,11 +4,26 @@ from .models import Tracked, ConfigParameter, StreamPhoto
 from django.contrib.auth.models import Group
 admin.site.unregister(Group)
 
-admin.site.register(StreamPhoto)
+@admin.register(StreamPhoto)
+class StreamPhoto(admin.ModelAdmin):
+    list_display = ('id', 'Cam_MxID', 'image')
+    list_display_links = ['id', 'Cam_MxID']
+    search_fields = ('id', 'Cam_MxID')
+    list_filter = ['Cam_MxID']
 
-admin.site.register(ConfigParameter)
 
+@admin.register(ConfigParameter)
+class ConfigParameter(admin.ModelAdmin):
+    list_display = ('id', 'Cam_MxID')
+    list_display_links = ['id', 'Cam_MxID']
+    list_filter = ['Cam_MxID' ]
+    search_fields = ('id', 'Cam_MxID')
+    
+
+@admin.register(Tracked)
 class TrackedModel(admin.ModelAdmin):
     list_display = ('Cam_MxID', 'incoming', 'outgoing', 'present', 'created_at',)
 
-admin.site.register(Tracked, TrackedModel)
+    search_fields = ('id', 'Cam_MxID')
+
+    list_filter = ['Cam_MxID']
