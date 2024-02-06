04d92a1d21650dddf4e9078584f30d1ef4a7c0c2
Fakhrillo <dombit1999@gmail.com>
2024-02-06 12:54:55 +0500

    new endpoint

--- result/views/result.py
+++ result/views/result.py
@@ -4,6 +4,9 @@ from ..serializers import ResultSerializer
 from rest_framework.response import Response 
 from rest_framework.generics import  CreateAPIView
 
+from datetime import datetime, timedelta
+from django.utils import timezone
+
 from rest_framework_simplejwt.authentication import  JWTAuthentication
 from rest_framework.permissions import IsAdminUser
 
@@ -18,9 +21,20 @@ class GetTrackedAPIView(APIView):
 
     def get(self, request, *args, **kwargs):
         Cam_MxID = self.kwargs.get('Cam_MxID')
-        data = Tracked.objects.filter(Cam_MxID=Cam_MxID).order_by('-id')
+        
+        start_date_str = request.query_params.get('start_date')
+        end_date_str = request.query_params.get('end_date')
+
+        if not (start_date_str and end_date_str):
+            today = timezone.now().date()
+            start_date = datetime.combine(today, datetime.min.time())
+            end_date = datetime.combine(today, datetime.max.time())
+        else:
+            start_date = datetime.strptime(start_date_str, '%Y-%m-%d')
+            end_date = datetime.strptime(end_date_str, '%Y-%m-%d')
+
+        data = Tracked.objects.filter(Cam_MxID=Cam_MxID, created_at__range=(start_date, end_date)).order_by('-id')
+        
         serializer = ResultSerializer(data, many=True)
         return Response(serializer.data)
         
-        
-
